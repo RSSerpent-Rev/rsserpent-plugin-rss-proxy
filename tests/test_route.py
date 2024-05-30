b491f5a495191ef53a26e9cd960fe2fbd48e215e
EkkoG <beijiu572@gmail.com>
2024-05-30 11:07:26 +0800

    fix: tests

--- tests/test_route.py
+++ tests/test_route.py
@@ -3,7 +3,7 @@ from starlette.testclient import TestClient
 
 def test_route(client: TestClient) -> None:
     """Test `rsserpent_plugin_rss_proxy.route`."""
-    response = client.get("/rss-proxy")
+    response = client.get("/proxy/https://developer.apple.com/news/rss/news.rss")
     assert response.status_code == 200
     assert response.headers["Content-Type"] == "application/xml"
-    assert response.text.count("Example Title") == 1
+    assert response.text.count("Latest News - Apple Developer") == 1
